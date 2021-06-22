b24983f40087e45282a5ed784d565a1f68dab99b
dbrakenhoff <d.brakenhoff@artesia-water.nl>
2021-06-22 11:18:49 +0200

    refactor

--- tests/test_003_pastastore.py
+++ tests/test_003_pastastore.py
@@ -12,127 +12,134 @@ with warnings.catch_warnings():
 
 
 @pytest.mark.dependency()
-def test_create_model(prj):
-    ml = prj.create_model("oseries1")
+def test_create_model(pstore):
+    ml = pstore.create_model("oseries1")
     return ml
 
 
 @pytest.mark.dependency()
-def test_properties(prj):
-    _ = prj.oseries
-    _ = prj.stresses
-    _ = prj.models
+def test_properties(pstore):
+    _ = pstore.oseries
+    _ = pstore.stresses
+    _ = pstore.models
     return
 
 
 @pytest.mark.dependency()
-def test_store_model(request, prj):
-    depends(request, [f"test_create_model[{prj.type}]"])
-    ml = test_create_model(prj)
-    prj.conn.add_model(ml)
+def test_store_model(request, pstore):
+    depends(request, [f"test_create_model[{pstore.type}]"])
+    ml = test_create_model(pstore)
+    pstore.conn.add_model(ml)
     return
 
 
 @pytest.mark.dependency()
-def test_store_model_missing_series(request, prj):
-    depends(request, [f"test_create_model[{prj.type}]",
-                      f"test_store_model[{prj.type}]"])
-    ml = test_create_model(prj)
-    o = prj.get_oseries("oseries1")
-    meta = prj.get_metadata("oseries", "oseries1", as_frame=False)
-    prj.del_models("oseries1")
-    prj.del_oseries("oseries1")
+def test_store_model_missing_series(request, pstore):
+    depends(request, [f"test_create_model[{pstore.type}]",
+                      f"test_store_model[{pstore.type}]"])
+    ml = test_create_model(pstore)
+    o = pstore.get_oseries("oseries1")
+    meta = pstore.get_metadata("oseries", "oseries1", as_frame=False)
+    pstore.del_models("oseries1")
+    pstore.del_oseries("oseries1")
     try:
-        prj.add_model(ml)
+        pstore.add_model(ml)
     except LookupError:
-        prj.add_oseries(o, "oseries1", metadata=meta)
-        prj.add_model(ml)
+        pstore.add_oseries(o, "oseries1", metadata=meta)
+        pstore.add_model(ml)
         return
 
 
 @pytest.mark.dependency()
-def test_get_model(request, prj):
-    depends(request, [f"test_create_model[{prj.type}]",
-                      f"test_store_model[{prj.type}]",
-                      f"test_store_model_missing_series[{prj.type}]"])
-    ml = prj.conn.get_models("oseries1")
+def test_get_model(request, pstore):
+    depends(request, [f"test_create_model[{pstore.type}]",
+                      f"test_store_model[{pstore.type}]",
+                      f"test_store_model_missing_series[{pstore.type}]"])
+    ml = pstore.conn.get_models("oseries1")
     return ml
 
 
 @pytest.mark.dependency()
-def test_del_model(request, prj):
-    depends(request, [f"test_create_model[{prj.type}]",
-                      f"test_store_model[{prj.type}]",
-                      f"test_store_model_missing_series[{prj.type}]",
-                      f"test_get_model[{prj.type}]"])
-    prj.conn.del_models("oseries1")
+def test_del_model(request, pstore):
+    depends(request, [f"test_create_model[{pstore.type}]",
+                      f"test_store_model[{pstore.type}]",
+                      f"test_store_model_missing_series[{pstore.type}]",
+                      f"test_get_model[{pstore.type}]"])
+    pstore.conn.del_models("oseries1")
     return
 
 
 @pytest.mark.dependency()
-def test_create_models(prj):
-    mls = prj.create_models_bulk(["oseries1", "oseries2"], store=True,
-                                 progressbar=False)
-    _ = prj.conn.models
+def test_create_models(pstore):
+    mls = pstore.create_models_bulk(["oseries1", "oseries2"], store=True,
+                                    progressbar=False)
+    _ = pstore.conn.models
     return mls
 
 
 @pytest.mark.dependency()
-def test_get_parameters(request, prj):
-    depends(request, [f"test_create_models[{prj.type}]"])
-    p = prj.get_parameters(progressbar=False, param_value="initial")
+def test_get_parameters(request, pstore):
+    depends(request, [f"test_create_models[{pstore.type}]"])
+    p = pstore.get_parameters(progressbar=False, param_value="initial")
     assert p.index.size == 2
     assert p.isna().sum().sum() == 0
     return p
 
 
 @pytest.mark.dependency()
-def test_solve_models_and_get_stats(request, prj):
-    depends(request, [f"test_create_models[{prj.type}]"])
-    mls = prj.solve_models(["oseries1", "oseries2"],
-                           ignore_solve_errors=False,
-                           progressbar=False,
-                           store_result=True)
-    stats = prj.get_statistics(["evp", "aic"], progressbar=False)
+def test_solve_models_and_get_stats(request, pstore):
+    depends(request, [f"test_create_models[{pstore.type}]"])
+    mls = pstore.solve_models(["oseries1", "oseries2"],
+                              ignore_solve_errors=False,
+                              progressbar=False,
+                              store_result=True)
+    stats = pstore.get_statistics(["evp", "aic"], progressbar=False)
     assert stats.index.size == 2
     return mls, stats
 
 
 @pytest.mark.dependency()
-def test_save_and_load_model(request, prj):
-    ml = prj.create_model("oseries3")
-    sm = ps.StressModel(prj.get_stresses('well1'), ps.Hantush,
+def test_save_and_load_model(request, pstore):
+    ml = pstore.create_model("oseries3")
+    sm = ps.StressModel(pstore.get_stresses('well1'), ps.Hantush,
                         name='well1', settings="well")
     ml.add_stressmodel(sm)
     ml.solve(tmin='1993-1-1')
     evp_ml = ml.stats.evp()
-    prj.add_model(ml, overwrite=True)
-    ml2 = prj.get_models(ml.name)
+    pstore.add_model(ml, overwrite=True)
+    ml2 = pstore.get_models(ml.name)
     evp_ml2 = ml2.stats.evp()
     assert allclose(evp_ml, evp_ml2)
     assert pst.util.compare_models(ml, ml2)
     return ml, ml2
 
 # @pytest.mark.dependency()
-# def test_model_results(request, prj):
-#     depends(request, [f"test_create_models[{prj.type}]",
-#                       f"test_solve_models[{prj.type}]"])
-#     prj.model_results(["oseries1", "oseries2"], progressbar=False)
+# def test_model_results(request, pstore):
+#     depends(request, [f"test_create_models[{pstore.type}]",
+#                       f"test_solve_models[{pstore.type}]"])
+#     pstore.model_results(["oseries1", "oseries2"], progressbar=False)
 #     return
 
 
-def test_oseries_distances(prj):
-    _ = prj.get_nearest_oseries()
+def test_oseries_distances(pstore):
+    _ = pstore.get_nearest_oseries()
     return
 
 
-def test_repr(prj):
-    return prj.__repr__()
+def test_repr(pstore):
+    return pstore.__repr__()
 
 
-def test_to_from_zip(prj):
-    zipname = f"test_{prj.type}.zip"
-    prj.to_zip(zipname, progressbar=False)
+def test_copy_dbase(pstore):
+    conn2 = pst.DictConnector("destination")
+    pst.util.copy_database(pstore.conn, conn2, overwrite=False,
+                           progressbar=True)
+    return
+
+
+def test_to_from_zip(pstore):
+    zipname = f"test_{pstore.type}.zip"
+    pstore.to_zip(zipname, progressbar=False)
     conn = pst.DictConnector("test")
     try:
         store = pst.PastaStore.from_zip(zipname, conn)
@@ -142,6 +149,6 @@ def test_to_from_zip(prj):
     return store
 
 
-def test_delete_db(prj):
-    pst.util.delete_pastastore(prj)
+def test_delete_db(pstore):
+    pst.util.delete_pastastore(pstore)
     return
