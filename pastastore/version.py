9b2daa0d7b31abf9a2c1523130991bbbfb5808e9
dbrakenhoff <d.brakenhoff@artesia-water.nl>
2023-01-19 14:44:54 +0100

    check pastas version

--- pastastore/version.py
+++ pastastore/version.py
@@ -1,1 +1,8 @@
+from importlib.metadata import version
+
+from packaging.version import parse as parse_version
+
+PASTAS_VERSION = version("pastas")
+PASTAS_LEQ_021 = parse_version(PASTAS_VERSION) <= parse_version("0.21.0")
+
 __version__ = "0.10b.0"
