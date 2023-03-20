1ed5b02eb3ba224861e25f30a58873cb3f1ee36a
dbrakenhoff <d.brakenhoff@artesia-water.nl>
2023-03-20 17:55:25 +0100

    fix #87 - use stored names as index, always - disregard stored name in metadata

--- pastastore/base.py
+++ pastastore/base.py
@@ -1244,10 +1244,7 @@ class ConnectorUtil:
             meta = pd.DataFrame(metalist)
         elif len(metalist) == 0:
             meta = pd.DataFrame()
-        if "name" in meta.columns:
-            meta.set_index("name", inplace=True)
-        else:
-            meta.index = names
+        meta.index = names
         return meta
 
     def _parse_model_dict(self, mdict: dict, update_ts_settings: bool = False):
