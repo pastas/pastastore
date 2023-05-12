7461b4708e08124fa8e5ac69830a10912a2e8224
dbrakenhoff <d.brakenhoff@artesia-water.nl>
2023-05-12 14:17:53 +0200

    up version

--- pastastore/version.py
+++ pastastore/version.py
@@ -4,4 +4,4 @@ from packaging.version import parse as parse_version
 PASTAS_VERSION = parse_version(ps.__version__)
 PASTAS_LEQ_022 = PASTAS_VERSION <= parse_version("0.22.0")
 
-__version__ = "1.2.0"
+__version__ = "1.2.1"
