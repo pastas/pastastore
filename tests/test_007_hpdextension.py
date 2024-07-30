7b9892b619ddf991c38aececd3c589aaf42d913e
Davíd Brakenhoff <d.brakenhoff@artesia-water.nl>
2024-07-30 17:02:12 +0200

    only attempt importing extensions when tests are run

--- tests/test_007_hpdextension.py
+++ tests/test_007_hpdextension.py
@@ -2,11 +2,12 @@
 import pytest
 
 import pastastore as pst
-from pastastore.extensions import activate_hydropandas_extension
 
 
 @pytest.mark.pastas150()
 def test_hpd_download_from_bro():
+    from pastastore.extensions import activate_hydropandas_extension
+
     activate_hydropandas_extension()
     pstore = pst.PastaStore()
     pstore.hpd.download_bro_gmw(
@@ -17,6 +18,8 @@ def test_hpd_download_from_bro():
 
 @pytest.mark.pastas150()
 def test_hpd_download_precipitation_from_knmi():
+    from pastastore.extensions import activate_hydropandas_extension
+
     activate_hydropandas_extension()
     pstore = pst.PastaStore()
     pstore.hpd.download_knmi_precipitation(
@@ -27,6 +30,8 @@ def test_hpd_download_precipitation_from_knmi():
 
 @pytest.mark.pastas150()
 def test_hpd_download_evaporation_from_knmi():
+    from pastastore.extensions import activate_hydropandas_extension
+
     activate_hydropandas_extension()
     pstore = pst.PastaStore()
     pstore.hpd.download_knmi_evaporation(
