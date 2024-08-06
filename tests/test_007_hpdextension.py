1c96b1cfc2b64378ed64b63639a3acbae96c1edd
Davíd Brakenhoff <d.brakenhoff@artesia-water.nl>
2024-08-06 11:40:34 +0200

    expand tests to check update functions as well

--- tests/test_007_hpdextension.py
+++ tests/test_007_hpdextension.py
@@ -1,5 +1,6 @@
 # ruff: noqa: D100 D103
 import pytest
+from pandas import Timestamp
 
 import pastastore as pst
 
@@ -38,3 +39,28 @@ def test_hpd_download_evaporation_from_knmi():
         stns=[260], tmin="2022-01-01", tmax="2022-01-31"
     )
     assert pstore.n_stresses == 1
+
+
+@pytest.mark.pastas150()
+def test_update_oseries():
+    from pastastore.extensions import activate_hydropandas_extension
+
+    activate_hydropandas_extension()
+
+    pstore = pst.PastaStore.from_zip("tests/data/test_hpd_update.zip")
+    pstore.hpd.update_bro_gmw(tmax="2024-01-31")
+    tmintmax = pstore.get_tmin_tmax("oseries")
+    assert tmintmax.loc["GMW000000036319_1", "tmax"] >= Timestamp("2024-01-30")
+    assert tmintmax.loc["GMW000000036327_1", "tmax"] >= Timestamp("2024-01-20")
+
+
+@pytest.mark.pastas150()
+def test_update_stresses():
+    from pastastore.extensions import activate_hydropandas_extension
+
+    activate_hydropandas_extension()
+
+    pstore = pst.PastaStore.from_zip("tests/data/test_hpd_update.zip")
+    pstore.hpd.update_knmi_meteo(tmax="2024-01-31")
+    tmintmax = pstore.get_tmin_tmax("stresses")
+    assert (tmintmax["tmax"] >= Timestamp("2024-01-31")).all()
