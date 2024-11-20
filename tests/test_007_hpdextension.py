1f1518810c59c7c64dbcf9877f0553ca7d2c9b5d
Davíd Brakenhoff <d.brakenhoff@artesia-water.nl>
2024-11-20 17:28:26 +0100

    limit length of downloads

--- tests/test_007_hpdextension.py
+++ tests/test_007_hpdextension.py
@@ -25,7 +25,7 @@ def test_hpd_download_precipitation_from_knmi():
     activate_hydropandas_extension()
     pstore = pst.PastaStore()
     pstore.hpd.download_knmi_precipitation(
-        stns=[260], tmin="2022-01-01", tmax="2022-01-31"
+        stns=[260], meteo_var="RH", tmin="2022-01-01", tmax="2022-01-31"
     )
     assert pstore.n_stresses == 1
 
@@ -50,10 +50,10 @@ def test_update_oseries():
     activate_hydropandas_extension()
 
     pstore = pst.PastaStore.from_zip("tests/data/test_hpd_update.zip")
-    pstore.hpd.update_bro_gmw(tmax="2024-01-31")
+    pstore.hpd.update_bro_gmw(tmax="2022-02-28")
     tmintmax = pstore.get_tmin_tmax("oseries")
-    assert tmintmax.loc["GMW000000036319_1", "tmax"] >= Timestamp("2024-01-30")
-    assert tmintmax.loc["GMW000000036327_1", "tmax"] >= Timestamp("2024-01-20")
+    assert tmintmax.loc["GMW000000036319_1", "tmax"] >= Timestamp("2022-02-27")
+    assert tmintmax.loc["GMW000000036327_1", "tmax"] >= Timestamp("2022-02-27")
 
 
 @pytest.mark.xfail(reason="KNMI is being flaky, so allow this test to xfail/xpass.")
@@ -64,9 +64,9 @@ def test_update_stresses():
     activate_hydropandas_extension()
 
     pstore = pst.PastaStore.from_zip("tests/data/test_hpd_update.zip")
-    pstore.hpd.update_knmi_meteo(tmax="2024-01-31", normalize_datetime_index=False)
+    pstore.hpd.update_knmi_meteo(tmax="2022-02-28", normalize_datetime_index=True)
     tmintmax = pstore.get_tmin_tmax("stresses")
-    assert (tmintmax["tmax"] >= Timestamp("2024-01-31")).all()
+    assert (tmintmax["tmax"] >= Timestamp("2024-02-27")).all()
 
 
 @pytest.mark.xfail(reason="KNMI is being flaky, so allow this test to xfail/xpass.")
@@ -78,8 +78,10 @@ def test_nearest_stresses():
 
     pstore = pst.PastaStore.from_zip("tests/data/test_hpd_update.zip")
     pstore.hpd.download_nearest_knmi_precipitation(
-        "GMW000000036319_1", tmin="2024-01-01"
+        "GMW000000036319_1", tmin="2024-01-01", tmax="2024-01-31"
     )
     assert "RD_GROOT-AMMERS" in pstore.stresses_names
-    pstore.hpd.download_nearest_knmi_evaporation("GMW000000036319_1", tmin="2024-01-01")
+    pstore.hpd.download_nearest_knmi_evaporation(
+        "GMW000000036319_1", tmin="2024-01-01", tmax="2024-01-31"
+    )
     assert "EV24_CABAUW-MAST" in pstore.stresses_names
