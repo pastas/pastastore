caf4969ef224eadf055b48dbae3b2ac4cfc9c524
dbrakenhoff <d.brakenhoff@artesia-water.nl>
2025-10-16 16:04:27 +0200

    update inits

--- pastastore/extensions/__init__.py
+++ pastastore/extensions/__init__.py
@@ -1,14 +1,18 @@
 # ruff: noqa: D104 F401
+import logging
+
 from pastastore.extensions.accessor import (
     register_pastastore_accessor as register_pastastore_accessor,
 )
+
+logger = logging.getLogger(__name__)
 
 
 def activate_hydropandas_extension():
     """Register Plotly extension for pastas.Model class for interactive plotting."""
     from pastastore.extensions.hpd import HydroPandasExtension as _
 
-    print(
+    logger.info(
         "Registered HydroPandas extension in PastaStore class, "
         "e.g. `pstore.hpd.download_bro_gmw()`."
     )
