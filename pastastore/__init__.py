e1d7149519dcd4881e73d3dad1cf8801cf155313
dbrakenhoff <d.brakenhoff@artesia-water.nl>
2025-10-16 16:05:39 +0200

    update init

--- pastastore/__init__.py
+++ pastastore/__init__.py
@@ -1,4 +1,6 @@
 # ruff: noqa: F401 D104
+import logging
+
 from pastastore import connectors, styling, util
 from pastastore.connectors import (
     ArcticDBConnector,
@@ -6,9 +8,11 @@ from pastastore.connectors import (
     PasConnector,
 )
 from pastastore.store import PastaStore
+from pastastore.util import get_color_logger
 from pastastore.version import __version__, show_versions
 
+logger = get_color_logger("INFO", logger_name=__name__)
 try:
     from pastastore import extensions
 except ModuleNotFoundError:
-    print("Could not import extensions module. Update pastas to >=1.3.0!")
+    logging.warning("Could not import extensions module. Update pastas to >=1.3.0!")
