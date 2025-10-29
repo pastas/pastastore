79db574b82bf3affcc8c2427528cc93b84f0c3d2
Martin Vonk <66305055+martinvonk@users.noreply.github.com>
2025-10-29 16:40:13 +0100

    Update logging and docstring for HydroPandas extension

--- pastastore/extensions/__init__.py
+++ pastastore/extensions/__init__.py
@@ -1,14 +1,18 @@
 # ruff: noqa: D104 F401
+from logging import getLogger
+
 from pastastore.extensions.accessor import (
     register_pastastore_accessor as register_pastastore_accessor,
 )
 
+logger = getLogger(__file__)
+
 
 def activate_hydropandas_extension():
-    """Register Plotly extension for pastas.Model class for interactive plotting."""
+    """Register HydroPandas extension for downloading time series data."""
     from pastastore.extensions.hpd import HydroPandasExtension as _
 
-    print(
-        "Registered HydroPandas extension in PastaStore class, "
+    logger.info(
+        "Registered HydroPandas extension in PastaStore, "
         "e.g. `pstore.hpd.download_bro_gmw()`."
     )
