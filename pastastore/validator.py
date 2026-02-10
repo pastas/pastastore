0986192f1320a62d4a127ab92fe4854c6505ba36
dbrakenhoff <d.brakenhoff@artesia-water.nl>
2026-02-10 15:37:36 +0100

    add support for temp series in RechargeModel

--- pastastore/validator.py
+++ pastastore/validator.py
@@ -359,12 +359,12 @@ class Validator:
                 prec_evap_model,
                 [i[classkey] for i in ml["stressmodels"].values()],
             ).any():
-                series_names += [
-                    istress["name"]
-                    for sm in ml["stressmodels"].values()
-                    if sm[classkey] in prec_evap_model
-                    for istress in [sm["prec"], sm["evap"]]
-                ]
+                for sm in ml["stressmodels"].values():
+                    if sm[classkey] in prec_evap_model:
+                        for istress in [sm["prec"], sm["evap"]]:
+                            series_names.append(istress["name"])
+                        if "temp" in sm and sm["temp"]:
+                            series_names.append(sm["temp"]["name"])
 
         else:
             raise TypeError("Expected pastas.Model or dict!")
@@ -457,6 +457,8 @@ class Validator:
                 classkey = "class"
                 if sm[classkey] in prec_evap_model:
                     stresses = [sm["prec"], sm["evap"]]
+                    if "temp" in sm and sm["temp"]:
+                        stresses.append(sm["temp"])
                 elif sm[classkey] in ["WellModel"]:
                     stresses = sm["stress"]
                 elif "stress" in sm:
