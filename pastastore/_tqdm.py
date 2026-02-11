c0de21f0611dc0202f6557f0899473e5b333eecd
Ruben Caljé <r.calje@artesia-water.nl>
2026-02-11 17:40:00 +0100

    Add `  # noqa: F401`, just as the line above

--- pastastore/_tqdm.py
+++ pastastore/_tqdm.py
@@ -9,4 +9,4 @@ with warnings.catch_warnings():
         module="tqdm",
     )
     from tqdm.auto import tqdm  # noqa: F401
-    from tqdm.contrib.concurrent import process_map
+    from tqdm.contrib.concurrent import process_map  # noqa: F401
