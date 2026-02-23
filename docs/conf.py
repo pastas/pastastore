c1cd327f65892ad76e6b0ea94cb6bcbee54dece1
dbrakenhoff <d.brakenhoff@artesia-water.nl>
2026-02-23 16:57:31 +0100

    fix conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -115,7 +115,7 @@ html_theme_options = {
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
 html_css_files = [
-    'custom.css',
+    "custom.css",
 ]
 
 
