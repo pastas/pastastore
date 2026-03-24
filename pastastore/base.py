879bfe96771cb677343bfa6cfa197a9db6dff9f7
dbrakenhoff <d.brakenhoff@artesia-water.nl>
2026-03-24 17:24:01 +0100

    use _item_exits()

--- pastastore/base.py
+++ pastastore/base.py
@@ -138,7 +138,7 @@ class ConnectorUtil:
         # oseries
         if "series" not in mdict["oseries"]:
             name = str(mdict["oseries"]["name"])
-            if name not in self.oseries_names:
+            if self._item_exists("oseries", name):
                 msg = f"oseries '{name}' not present in library"
                 raise LookupError(msg)
             mdict["oseries"]["series"] = self.get_oseries(name).squeeze()
