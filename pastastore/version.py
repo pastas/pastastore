30bdc3c519a15ddbb12b42d31b51ef5ce8995a2d
dbrakenhoff <d.brakenhoff@artesia-water.nl>
2026-04-29 14:19:48 +0200

    up version

--- pastastore/version.py
+++ pastastore/version.py
@@ -9,7 +9,7 @@ PASTAS_VERSION = parse_version(ps.__version__)
 PASTAS_GEQ_150 = PASTAS_VERSION >= parse_version("1.5.0")
 PASTAS_GEQ_200 = PASTAS_VERSION >= parse_version("2.0.0")
 
-__version__ = "1.14.0.dev0"
+__version__ = "2.0.0"
 
 
 def show_versions(optional=False) -> None:
